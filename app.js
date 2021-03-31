bf4809417f4fa8367393e6141b013b9cac3678cb
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-03-31 01:30:36 -0500

    sucessfully commited

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var petRouter = require('./routes/pet');
 var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pet', petRouter);
 app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
